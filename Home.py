d33fe5b3d149c0784aa56165eabf5a9887f2a37c
Beexisuma <120141747+Beexisuma@users.noreply.github.com>
2023-01-30 18:25:01 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,6 +2,6 @@
 import streamlit as st
 
 
-st.write(""" # Stock price viewer and machine-learning based prediction site""")
+st.write(""" # Stock price viewer """)
 st.write("This is an early version, please be patient! (Made with *Streamlit*)")
 st.sidebar.markdown("# Home")
