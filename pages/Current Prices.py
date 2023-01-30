3564441619b126de1c69f75656d2869eac093345
Beexisuma <120141747+Beexisuma@users.noreply.github.com>
2023-01-30 18:18:33 -0500

    Update Current Prices.py

--- pages/Current Prices.py
+++ pages/Current Prices.py
@@ -17,16 +17,9 @@ textInput = st.text_input(
 )
 startDate = st.date_input("Start Date")
 endDate = st.date_input("End Date- Must be at least one day after Start Date")
-yes = st.checkbox("Would you like to receive email updates?")
+
 c1 = get_ticker(textInput)
 data = c1.history(period="3mo")
-if yes:
-    userEmail = st.text_input("Email Address", max_chars=100, label_visibility="visible")
-    userAnswer = st.selectbox("When the price is:", ("Greater Than", "Less Than"))
-    if userAnswer == "Greater Than":
-        greaterPrice = st.text_input("The Price to be notified of", max_chars=100, label_visibility="visible", placeholder="$10.00")
-    else:
-        lesserPrice = st.text_input("The Price to be notified of", max_chars=100, label_visibility="visible", placeholder="$10.00")
 
 # Gets a selected company's information on button press
 runButton = st.button("See Prices")
